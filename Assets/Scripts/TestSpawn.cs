ccdf4b59e66380b2f260c7d34cd386899c2479bd
osato18 <otuy.nin.121011@gmail.com>
2025-09-05 01:42:28 +0900

    一旦コミット

--- Assets/Scripts/TestSpawn.cs
+++ Assets/Scripts/TestSpawn.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
+
+[RequireComponent(typeof(ARRaycastManager))]
 public class TestSpawn : MonoBehaviour
 {
+
+    //ARRayCastのコード
+    public TrackableType type;
+
+    ARRaycastManager _raycastManager;
+    List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
+    //テスト
     [SerializeField] private GameObject _spawnObj;
     [SerializeField] private GameObject _offsetObj;
     // Start is called before the first frame update
     void Start()
     {
-
+        _raycastManager = GetComponent<ARRaycastManager>();
     }
 
     public void Spawn()
     {
-        Instantiate(_spawnObj, _offsetObj.transform.position, Quaternion.Euler(0,-90,0));  //オブジェクトの生成
+        if (_raycastManager.Raycast(Input.GetTouch(0).position, hitResults, TrackableType.AllTypes))
+        {
+            Instantiate(_spawnObj, hitResults[0].pose.position, Quaternion.Euler(0, -90, 0));  //オブジェクトの生成
+        }
     }
 
-        void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(_offsetObj.transform.position, 0.1f);
